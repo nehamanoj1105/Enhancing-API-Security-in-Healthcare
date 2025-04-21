c10fddf84e0c3ec11ddf5f0c9a0490bba826ba7b
Neha Manoj <143329337+nehamanoj1105@users.noreply.github.com>
2025-04-21 23:49:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,71 +1,19 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flasgger import Swagger
-import joblib
-import pandas as pd
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from flask import Flask
+from extensions import db
+from routes import routes
 
+app = Flask(__name__)
 
-db = SQLAlchemy()
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db.init_app(app)
 
-model = joblib.load("isolation_forest_model.pkl")
-scaler = joblib.load("scaler.pkl")
-label_encoder = joblib.load("label_encoder.pkl")
+app.register_blueprint(routes)
 
-def create_app():
-    app = Flask(__name__)
-    
-    
-    app.config['SECRET_KEY'] = 'your_very_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    
-    Swagger(app)
-    db.init_app(app)
-
-   
-    @app.route('/')
-    def home():
-        return "Welcome to the Anomaly Detection API!"
-
-    
-    @app.route('/predict', methods=['POST'])
-    def predict_anomaly():
-        data = request.get_json()
-        
-        
-        login_time = data['login_time']
-        failed_attempts = data['failed_attempts']
-        login_location = data['login_location']
-        
-       
-        input_data = pd.DataFrame([[login_time, failed_attempts, login_location]], columns=['login_time', 'failed_attempts', 'login_location'])
-        
-       
-        if isinstance(input_data['login_location'][0], str):
-            if login_location not in label_encoder.classes_:
-                input_data['login_location'] = 'Unknown'
-            input_data['login_location'] = label_encoder.transform([input_data['login_location'][0]])
-
-        
-        input_scaled = scaler.transform(input_data)
-
-       
-        prediction = model.predict(input_scaled)
-        
-       
-        if prediction[0] == -1:
-            return jsonify({"message": "Anomaly detected"}), 200
-        else:
-            return jsonify({"message": "Normal login"}), 200
-    
-    from routes import register_routes
-    register_routes(app)
-
-    return app
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
