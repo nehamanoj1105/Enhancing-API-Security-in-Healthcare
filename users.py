4e0c0c054c908f4e10145f6ab1b9d269d01112a7
Neha Manoj <143329337+nehamanoj1105@users.noreply.github.com>
2025-04-21 23:50:42 +0530

    Update users.py

--- users.py
+++ users.py
@@ -1,9 +1,12 @@
-from flask_sqlalchemy import SQLAlchemy
-from app import db
-
+from extensions import db 
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    role = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
